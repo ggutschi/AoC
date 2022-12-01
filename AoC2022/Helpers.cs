ef17afe3cfdc299d15d9b502b36e0c0012943037
Gernot Gutschi <g.gutschi@cellcreek.com>
2022-12-01 07:29:56 +0100

    Formatted output

--- AoC2022/Helpers.cs
+++ AoC2022/Helpers.cs
@@ -1,4 +1,6 @@
 ﻿using AoC2022.Days;
+using Pastel;
+using System.Drawing;
 
 namespace AoC2022;
 
@@ -6,18 +8,18 @@ public static class Helpers
 {    
     public static async Task WriteOutputAsync(IDay day)
     {
+        var answerPartOne = await day.CalculatePartOne();
+        var answerPartTwo = await day.CalculatePartTwo();
+
         Console.WriteLine($"""
-       Day {day.Number}
-       =====
+            {("Day " + day.Number).Pastel(Color.Yellow)}
 
-       Part One
-       --------
-       Answer is {await day.CalculatePartOne()}
-       
-       Part Two
-       --------
-       Answer is {await day.CalculatePartTwo()}
+            Part One
+            {"Answer is".Pastel(Color.Gray)} {answerPartOne.Pastel(Color.Lime)}
+           
+            Part Two
+            {"Answer is".Pastel(Color.Gray)} {answerPartTwo.Pastel(Color.Lime)}
 
-       """);
+            """);
     }
 }
